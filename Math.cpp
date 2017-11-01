d371fff89a754cdeb9c427af70a3c4ac5c4563ff
hadoth <pokomeda.karol@gmail.com>
2017-11-01 18:56:59 +0100

    Remove class implementation

--- Math.cpp
+++ Math.cpp
@@ -1,18 +1,16 @@
 #include "Math.h"
 
-namespace Zmpo {
-    unsigned int Math::power(unsigned int x, unsigned int n) {
-        unsigned int partialPower;
+unsigned int Math::power(unsigned int x, unsigned int n) {
+    unsigned int partialPower;
 
-        if (n == 0) {
-            return 1;
+    if (n == 0) {
+        return 1;
+    } else {
+        partialPower = Math::power(x, n / 2);
+        if (n % 2 == 0) {
+            return partialPower * partialPower;
         } else {
-            partialPower = power(x, n / 2);
-            if (n % 2 == 0) {
-                return partialPower * partialPower;
-            } else {
-                return x * partialPower * partialPower;
-            }
+            return x * partialPower * partialPower;
         }
     }
 }
--- Math.h
+++ Math.h
@@ -1,15 +1,7 @@
-//
-// Created by kpokomeda on 30.10.17.
-//
-
 #ifndef L1E2_ZMPOMATH_H
 #define L1E2_ZMPOMATH_H
-namespace Zmpo {
-    class Math {
-    public:
-        static unsigned int power(unsigned int x, unsigned int n);
-    };
+namespace Math {
+    unsigned int power(unsigned int x, unsigned int n);
 }
-
 
 #endif //L1E2_ZMPOMATH_H
